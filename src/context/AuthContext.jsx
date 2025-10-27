411578d1d8d8efa1a461e52cbe65cd7f0ccd32b6
Elvis Nwogu <nwoguelvis92@gmail.com>
2025-10-27 14:27:03 +0100

    Few extra changes

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -35,7 +35,7 @@ export const AuthProvider = ({ children }) => {
   // Login
   const login = async (email, password) => {
     try {
-      const res = await axios.post('https://osusu-backend37us.onrender.com/api/users/login', {
+      const res = await axios.post('https://osusu-backend-37us.onrender.com/api/users/login', {
         email,
         password,
       });
@@ -62,7 +62,7 @@ export const AuthProvider = ({ children }) => {
   // Signup
   const signup = async (name, email, password) => {
     try {
-      const res = await axios.post("http://localhost:4009/api/users/register", {
+      const res = await axios.post("https://osusu-backend-37us.onrender.com/api/users/register", {
         name,
         email,
         password,
